be6025da1149b05079605186b956c618295b1777
Pradyun Gedam <pradyunsg@users.noreply.github.com>
2021-03-14 14:39:49 +0000

    First attempt at the skeleton

--- src/sphinx_basic_ng/__init__.py
+++ src/sphinx_basic_ng/__init__.py
@@ -7,14 +7,30 @@ import sphinx
 from typing import Any, Dict
 
 
-THEME_PATH = (Path(__file__).parent / "theme" / "basic-ng").resolve()
+_THEME_PATH = (Path(__file__).parent / "theme" / "basic-ng").resolve()
+
+
+def _html_page_context(
+    app: sphinx.application.Sphinx,
+    pagename: str,
+    templatename: str,
+    context: Dict[str, Any],
+    doctree: Any,
+) -> None:
+    if "content_width" not in context:
+        context["content_width"] = "40rem"
+    if "sidebar_width" not in context:
+        context["sidebar_width"] = "15rem"
 
 
 def setup(app: sphinx.application.Sphinx) -> Dict[str, Any]:
     """Entry point for sphinx theming."""
     app.require_sphinx("3.0")
 
-    app.add_html_theme("basic-ng", str(THEME_PATH))
+    app.add_html_theme("basic-ng", str(_THEME_PATH))
+    app.add_css_file("skeleton.css")
+
+    app.connect("html-page-context", _html_page_context)
 
     return {
         "parallel_read_safe": True,
