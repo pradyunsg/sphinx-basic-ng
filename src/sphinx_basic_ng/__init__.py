56932fdf0dc7f66319e4cc6bc6114bee24b790cc
Pradyun Gedam <pradyunsg@users.noreply.github.com>
2022-06-27 21:59:19 +0100

    Bump up to Sphinx 4
    This is a reasonably recent version of Sphinx, and also what the
    dependency constraint points to.

--- src/sphinx_basic_ng/__init__.py
+++ src/sphinx_basic_ng/__init__.py
@@ -12,7 +12,7 @@ _THEME_PATH = (Path(__file__).parent / "theme" / "basic-ng").resolve()
 
 def setup(app: sphinx.application.Sphinx) -> Dict[str, Any]:
     """Entry point for sphinx theming."""
-    app.require_sphinx("3.0")
+    app.require_sphinx("4.0")
 
     app.add_html_theme("basic-ng", str(_THEME_PATH))
 
